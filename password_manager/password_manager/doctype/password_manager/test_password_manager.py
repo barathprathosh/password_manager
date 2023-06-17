ee9ce15fb010a11bc188ad1e85342f02802e343b
barathprathosh <barathprathosh@gmail.com>
2023-06-17 17:26:48 +0530

    password management doctype added

---
+++ password_manager/password_manager/doctype/password_manager/test_password_manager.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, barathprathosh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPasswordManager(FrappeTestCase):
+	pass
---
+++ password_manager/password_manager/doctype/pm_category/pm_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, barathprathosh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PM Category', {
+	// refresh: function(frm) {
+
+	// }
+});
