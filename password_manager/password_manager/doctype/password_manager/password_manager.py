9fe8e28cd4072d55da0daa56bbab7cc031942f6c
Barath Prathosh <barathprathosh@gmail.com>
2024-02-07 17:53:37 +0530

    Update password_manager.py

--- password_manager/password_manager/doctype/password_manager/password_manager.py
+++ password_manager/password_manager/doctype/password_manager/password_manager.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2023, barathprathosh and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import date
 from frappe.model.document import Document
 
 class PasswordManager(Document):
